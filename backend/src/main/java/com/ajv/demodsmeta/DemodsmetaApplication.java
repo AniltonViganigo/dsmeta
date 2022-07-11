00685ca6dba27f2103a7e2e51606ae0ea4edbde8
One4_Projects <one4repository@gmail.com>
2022-07-11 17:04:48 -0300

    Project Created

---
+++ backend/src/main/java/com/ajv/demodsmeta/DemodsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.ajv.demodsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ajv/demodsmeta/DemodsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajv.demodsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
